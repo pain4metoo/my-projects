8898ce3b5031868c4f52d05ddef3dcf171e975f8
pain4metoo <pain4metoo@gmail.com>
2023-04-03 16:34:54 +0200

    feat: add fonts and change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,18 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: ['babel-loader', 'ts-loader'],
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
             }
         ]
     },

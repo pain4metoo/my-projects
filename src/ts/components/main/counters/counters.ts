4dcab8a9d7bdc26706d84a053a3d38a6e94d0403
pain4metoo <pain4metoo@gmail.com>
2023-04-17 15:20:47 +0200

    feat: add counters component

--- src/ts/components/main/counters/counters.ts
+++ src/ts/components/main/counters/counters.ts
@@ -4,5 +4,12 @@ import './counter.scss';
 export class Counters extends Control {
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'section', 'main_counters');
+    const movesBlock = new Control(this.node, 'div', 'main_counters_moves', '');
+    const movesBlockText = new Control(movesBlock.node, 'p', 'main_counters_text', 'Moves: ');
+    const movesBlockNumber = new Control(movesBlock.node, 'p', 'main_counters_number', '0');
+
+    const timeBlock = new Control(this.node, 'div', 'main_counters_time', '');
+    const timeBlockText = new Control(timeBlock.node, 'p', 'main_counters_time_text', 'Time: ');
+    const timeBlockNumber = new Control(timeBlock.node, 'p', 'main_counters_time_number', '00:00');
   }
 }

b7a61bbe60df65818b5d00e32065901232797a9f
pain4metoo <pain4metoo@gmail.com>
2023-09-22 08:35:09 +0300

    fix: adaptive and collect song

--- src/ts/components/app.ts
+++ src/ts/components/app.ts
@@ -1,15 +1,33 @@
 import Control from '../common/control';
 import { state } from '../common/state';
+import { StateOptions } from '../common/state-types';
 
 import { Footer } from './footer/footer';
 import { Header } from './header/header';
 import { Main } from './main/main';
 
 export class App extends Control {
+  private appListener: (type: StateOptions) => void;
   constructor(parentNode: HTMLElement) {
     super(parentNode, 'div', 'wrapper');
     const header = new Header(this.node);
     const main = new Main(this.node);
     const footer = new Footer(this.node);
+
+    this.appListener = (type: StateOptions) => {
+      switch (type) {
+        case StateOptions.changeLanguage:
+          this.changeFontFamily(state.getLanguage(), parentNode);
+          break;
+      }
+    };
+
+    state.onUpdate.add(this.appListener);
+  }
+
+  private changeFontFamily(isEn: boolean, parentNode: HTMLElement) {
+    isEn
+      ? (parentNode.style.fontFamily = `'Thasadith', sans-serif`)
+      : (parentNode.style.fontFamily = `'Ubuntu', sans-serif`);
   }
 }

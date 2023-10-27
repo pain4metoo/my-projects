6668e2e04e753db3d9c97ff740f436ffc4f879ba
pain4metoo <pain4metoo@gmail.com>
2023-10-27 19:23:15 +0300

    fix: bug with error

--- src/ts/components/main/game/soundControl.ts
+++ src/ts/components/main/game/soundControl.ts
@@ -94,12 +94,14 @@ export class SoundControl {
   private play(): void {
     // check is Safari or not cuz in the Safari oncanplay doesn't work;
     if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
-      this.sound.onloadedmetadata = (): void => {
+      this.sound.oncanplaythrough = (): void => {
         this.sound.play();
       };
     } else {
       this.sound.oncanplay = (): void => {
-        this.sound.play();
+        this.sound.play().catch((error: string) => {
+          throw Error(error);
+        });
       };
     }
   }

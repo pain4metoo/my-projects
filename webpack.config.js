f7d4093c0062d05c3353655e21a26f2e33f6f5bf
pain4metoo <pain4metoo@gmail.com>
2023-11-14 12:51:12 +0300

    fix: prettier and eslint settings

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src', 'index.html');
-    })
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
